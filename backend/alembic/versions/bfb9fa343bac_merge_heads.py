059aa225d28852e3d0ee0c46b0a2004047dfa228
Anshereina Sanchez <maanshereinapsanchez@iskolarngbayan.pup.edu.ph>
2025-11-04 21:59:10 +0800

    chore(db): merge Alembic heads

---
+++ backend/alembic/versions/bfb9fa343bac_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: bfb9fa343bac
+Revises: ae99c3d266d5, f1a2b3c4d5e6
+Create Date: 2025-11-04 16:19:00.591562
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bfb9fa343bac'
+down_revision = ('ae99c3d266d5', 'f1a2b3c4d5e6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass 
